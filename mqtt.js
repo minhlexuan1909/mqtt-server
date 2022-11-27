4c3bac4f672023b0238ffa7eb43352a80395947b
minhlexuan1909 <minhlx1909@gmail.com>
2022-11-27 20:55:59 +0700

    add clg

--- mqtt.js
+++ mqtt.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 1883;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
+console.log("Hi " + PORT);
+
 httpServer.listen(PORT, function () {
   console.log("websocket server listening on port ", PORT);
 });
